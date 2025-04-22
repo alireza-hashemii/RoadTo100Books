1a84722363968f7a64f3456740cea78bc8960f0a
alireza-hashemii <alirezahashemi.dev@gmail.com>
2025-04-22 13:59:50 +0330

    queue - implementation

--- 1.Algorithms-DataStructures-RobertLafore/4.stacks_queues.py
+++ 1.Algorithms-DataStructures-RobertLafore/4.stacks_queues.py
@@ -34,3 +34,60 @@ class Stack:
     
     def __len__(self):
         return self.__top + 1
+    
+
+
+
+class Queue:
+    def __init__(self, size):
+        self.__maxsize = size
+        self.__rear = 0
+        self.__front = 1
+        self.__que = [None] * size
+        self.__nitems = 0
+
+    def insert(self, item):
+        if self.isfull():
+            raise Exception('The queue is already reached its maximum size')
+        self.__rear += 1 
+        if self.__rear == self.__maxsize:
+            self.__rear = 0
+        self.__que[self.__rear] = item
+        self.__nitems += 1
+        return True
+    
+    def remove(self):
+        if self.isempty():
+            raise Exception("Not possible to remove an item from an empty queue")
+        
+        front = self.__que[self.__front]
+        self.__que[self.__front] = None
+        self.__front += 1 
+        if self.__front == self.__maxsize:
+            self.__front = 0
+        self.__nitems -= 1
+        return front
+    
+    def peek(self):
+        return None if self.isempty() else self.__que[self.__front]
+
+    def isempty(self):
+        return len(self.__que) == 0
+    
+    def isfull(self):
+        return len(self.__que) == self.__maxsize
+    
+    def __len__(self): 
+        return self.__nItems
+     
+    def __str__(self): # Convert queue to string
+        ans = "[" # Start with left bracket
+        for i in range(self.__nitems): # Loop through current items
+            if len(ans) > 1: # Except next to left bracket,
+                    ans += ", " # separate items with comma
+            j = i + self.__front # Offset from front
+            if j >= self.__maxSize: # Wrap around circular array
+                j -= self.__maxSize
+            ans += str(self.__que[j]) # Add string form of item
+        ans += "]" # Close with right bracket
+        return ans
